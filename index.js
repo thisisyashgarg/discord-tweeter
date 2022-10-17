f97d8e6bd459e57508657da35886b063fcb4d7a3
YashGarg220 <yash.garg2702@gmail.com>
2022-10-17 10:26:56 +0530

    code cleanup done

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const dotenv = require("dotenv");
-dotenv.config();
-const Twit = require("twit");
+const twit = require("twit");
+require('dotenv').config()
 
-const T = new Twit({
+const credentials = new twit({
     consumer_key: process.env.API_KEY,
     consumer_secret: process.env.API_SECRET,
     access_token: process.env.ACCESS_TOKEN,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   });
 
-const prefix = 'Tweet my message:'
+const tweetCall = 'Tweet my message:'
 
-client.on('message', (msg) => {
+client.on('message', (message) => {
 
-    const tweet = () => {
-        const text = msg.content.substring(18);
-        const onFinish = (err, reply) => {
-          if (err) {
-            console.log("Error: ", err.message);
-          } else {
-            console.log("Success: ", reply);
-          }
-        };
-        T.post("statuses/update", { status: text }, onFinish);
+    const tweeterFunction = () => {
+        const tweetToBeSent = message.content.substring(18);
+        credentials.post("statuses/update", { status: tweetToBeSent });
       };
     
-    if (msg.content.startsWith(prefix) && msg.content.length <= 298){
-        msg.reply('Woohoo, your tweet has been sent');
-        tweet();
+    if (message.content.startsWith(tweetCall) && message.content.substring(18).length <= 280){
+        message.reply('Woohoo, your tweet has been sent :)');
+        tweeterFunction();
     }
     
-    if(msg.content.length > 298){
-      msg.reply('Only 280 characters are allowed');
-      
+    if(message.content.substring(18).length > 280){
+      message.reply('Sorry, but twiiter allows only 280 characters');
     }
-
   });
 
 client.login(process.env.TOKEN);
